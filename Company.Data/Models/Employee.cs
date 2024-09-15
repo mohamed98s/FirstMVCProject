237b9f548def73475f3344678a54ec942e210082
mohamed98s <moh.ori398@gmail.com>
2024-09-15 16:52:31 +0300

    Part 06 Build Entities(Employee&Department)

---
+++ Company.Data/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data.Models
+{
+    public class Employee:BaseEntity
+    {
+        public string name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public decimal Salary { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime HiringDate { get; set; }
+        public string ImgUrl { get; set; }
+        public Department Department { get; set; }
+        public int? DepartmentId { get; set; }
+
+    }
+}
